57ac06019fbba9bac2acb02a46c8c0c2b9ae4e63
TheDogMaster <timengelbertink01@gmail.com>
2026-06-18 13:04:53 +0200

    minigame settings in seperate script to change with rarity

--- FishCatting/Assets/Scripts/FishingMechanic/FishingMinigame.cs
+++ FishCatting/Assets/Scripts/FishingMechanic/FishingMinigame.cs
@@ -27,23 +27,14 @@ public class FishingMinigame : MonoBehaviour
 
     [Header("Fish slider settings")]
     [SerializeField]
-    private float fishSpeed = 1;
-    [SerializeField]
-    private int minTimeChange = 10;
-    [SerializeField]
-    private int maxTimeChange = 30;
-    [SerializeField]
     private RectTransform fishWinArea;
-    [SerializeField, Range(1, 100)]
-    private int fishWinSize;
 
-    [Header("Timer slider settings")]
     [SerializeField]
-    private float upTimerSpeed = 0.5f;
-    [SerializeField]
-    private float downTimerSpeed = 0.2f;
-    [SerializeField]
-    private float beginValue = 0.1f;
+    private FishingMinigameSettings[] miniGameSettings;
+
+    private FishingMinigameSettings usedSettings;
+    public float testSetSettings;
+
     private float timerToBegin = 0;
 
     private float destinationValue = 0;
@@ -57,7 +48,6 @@ public class FishingMinigame : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        fishWinArea.sizeDelta = new Vector2(0, fishWinSize);
         catchFish = GetComponent<CatchFishes>();
         //StartMiniGame();
     }
@@ -106,23 +96,23 @@ public class FishingMinigame : MonoBehaviour
         fishTimer += Time.deltaTime;
         if (fishTimer > fishSwitchTime || Input.GetKeyDown(KeyCode.Space))
         {
-            destinationValue = Random.Range(fishslider.minValue + (float)fishWinSize / 200, fishslider.maxValue - (float)fishWinSize / 200);
-            fishSwitchTime = Random.Range(minTimeChange, maxTimeChange);
+            destinationValue = Random.Range(fishslider.minValue + (float)usedSettings.fishWinSize / 200, fishslider.maxValue - (float)usedSettings.fishWinSize / 200);
+            fishSwitchTime = Random.Range(usedSettings.minTimeChange, usedSettings.maxTimeChange);
             fishTimer = 0;
             //Debug.Log(destinationValue);
             //Debug.Log(new Vector2(fishslider.minValue + (float)fishWinSize / 200, fishslider.maxValue - (float)fishWinSize / 200));
         }
-        if (Mathf.Abs(destinationValue - fishslider.value) > 0.01) fishslider.value += fishSpeed * Mathf.Sign(destinationValue - fishslider.value) * Time.deltaTime;
+        if (Mathf.Abs(destinationValue - fishslider.value) > 0.01) fishslider.value += usedSettings.fishSpeed * Mathf.Sign(destinationValue - fishslider.value) * Time.deltaTime;
     }
 
     private void TimerSlider()
     {
-        if (playerSlider.value + 0.05f > fishslider.value - (float)fishWinSize / 200 && playerSlider.value - 0.05f < fishslider.value + (float)fishWinSize / 200)
+        if (playerSlider.value + 0.05f > fishslider.value - (float)usedSettings.fishWinSize / 200 && playerSlider.value - 0.05f < fishslider.value + (float)usedSettings.fishWinSize / 200)
         {
-            timerSlider.value += upTimerSpeed * Time.deltaTime;
+            timerSlider.value += usedSettings.upTimerSpeed * Time.deltaTime;
             //Debug.Log("winning");
         }
-        else timerSlider.value -= downTimerSpeed * Time.deltaTime;
+        else timerSlider.value -= usedSettings.downTimerSpeed * Time.deltaTime;
         if (timerSlider.value == 0)
         {
             FinishMiniGame(false);
@@ -135,13 +125,25 @@ public class FishingMinigame : MonoBehaviour
 
     private void ResetMinigame()
     {
+        SetSettings();
+        fishWinArea.sizeDelta = new Vector2(0, usedSettings.fishWinSize);
         playerSlider.value = 0;
         timerToBegin = 0;
         fishTimer = 0;
         fishSwitchTime = 1f;
-        destinationValue = fishslider.minValue + (float)fishWinSize / 200;
+        destinationValue = fishslider.minValue + (float)usedSettings.fishWinSize / 200;
         fishslider.value = destinationValue;
-        timerSlider.value = beginValue;
+        timerSlider.value = usedSettings.beginValue;
+    }
+
+    private void SetSettings()
+    {
+        switch (testSetSettings)
+        {
+            case 0:
+                usedSettings = miniGameSettings[0];
+                break;
+        }
     }
 
     private void FinishMiniGame(bool ifWon)
